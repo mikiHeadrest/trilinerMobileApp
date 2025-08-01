8fbd37582b5645ec381ce362ef2714eae68239f5
mikiHeadrest <miguelangelopezlopezll@gmail.com>
2025-08-01 08:27:56 -0700

    Busqueda, Combo Box hechos

--- src/app/ventana-historial-movimientos/ventana-historial-movimientos.page.ts
+++ src/app/ventana-historial-movimientos/ventana-historial-movimientos.page.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonIcon, IonInput, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { time } from 'ionicons/icons';
+import { caretDownOutline, searchOutline, time } from 'ionicons/icons';
 
 @Component({
   selector: 'app-ventana-historial-movimientos',
   templateUrl: './ventana-historial-movimientos.page.html',
   styleUrls: ['./ventana-historial-movimientos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, IonInput]
 })
 export class VentanaHistorialMovimientosPage implements OnInit {
 
   constructor() {
-    addIcons({time})
+    addIcons({time,searchOutline, caretDownOutline})
   }
 
   ngOnInit() {
